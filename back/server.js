9d497ca10a80b7e352bb68ba5d91c8f4d1496273
POO <oscar.rvalencia@hotmail.com>
2022-10-19 17:18:22 -0500

    Conexion con servidor de BD

--- back/server.js
+++ back/server.js
@@ -1,8 +1,12 @@
 const app = require("./app");
+const connectDatabase = require("./config/databaseConfig");
 
 // Cofiguracion ruta dotenv
 const dotenv = require("dotenv");
 dotenv.config({path: "back/config/config.env"})
+
+// Llamado base de datos
+connectDatabase();
 
 // funcion de conexion
 const server = app.listen(process.env.PORT, () => {
