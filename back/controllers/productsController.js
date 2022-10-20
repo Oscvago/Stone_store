ac1eed3c0bcb72002ceea86a0f03688b41cdf753
POO <oscar.rvalencia@hotmail.com>
2022-10-19 19:25:14 -0500

    Creacion de modelos y productos en BD

--- back/controllers/productsController.js
+++ back/controllers/productsController.js
@@ -1,6 +1,19 @@
+const producto = require("../models/productos");
+
+// Ver el listado de productos
 exports.getProducts = (req, res, next) => {
     res.status(200).json({
         success:true,
         message: "Lista de consulta de productos"
+    })
+}
+
+// Funcion para crear producto
+exports.newProduct = async(req, res, next) => {
+    const product = await producto.create(req.body);
+
+    res.status(201).json({
+        success:true,
+        product
     })
 }
--- back/routes/productsRoute.js
+++ back/routes/productsRoute.js
@@ -2,9 +2,11 @@ const express = require("express")
 const router = express.Router();
 
 // Traer respuesta json del controlador
-const {getProducts} = require("../controllers/productsController");
+const {getProducts, newProduct} = require("../controllers/productsController");
 
 // Ruta en navegador para consutar producto
 router.route('/productos').get(getProducts)
+// Ruta en navegador para ccrear un producto
+router.route('/productos/nuevo').post(newProduct);
 
 module.exports = router;
