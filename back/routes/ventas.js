61ff5806f3a9987c811abdbbeee199a6646b8d7b
AIC <104699297+AngelaIC@users.noreply.github.com>
2022-11-12 03:33:53 -0500

    Delete back directory
    Signed-off-by: AIC <104699297+AngelaIC@users.noreply.github.com>

--- back/routes/ventas.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router();
-const {getVentas} = require("../controllers/ventasController")
-
-router.route("/ventas").get(getVentas)
-
-module.exports = router;
